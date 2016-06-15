f8b7172adfc929f1f006b11d2c8cb2d6967cb82d
nampord <Frank.Dropmann@concatus.ch>
2016-06-15 16:08:43 +0200

    testapp

---
+++ FTA/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\onedrive\visual studio 2015\Projects\FTA\FTA\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
